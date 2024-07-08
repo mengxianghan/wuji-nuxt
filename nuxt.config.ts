72726132b8f67572f3521a42cc571b24a4095886
mengxianghan <1056811341@qq.com>
2024-07-08 22:21:01 +0800

    feat: 增加 tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
-    modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
+    modules: [
+        '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/tailwindcss',
+    ],
     app: {
         head: {
             title: 'Wuji Nuxt!',
