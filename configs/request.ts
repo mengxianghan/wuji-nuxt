3dd989c80fa692b853cdb5f29e28a46b20a870f7
MengXianghan <1056811341@qq.com>
2024-07-11 19:03:07 +0800

    feat: 更换新缓存

--- configs/request.ts
+++ configs/request.ts
@@ -1,9 +1,9 @@
-export default () => {
+export default (): {server: string | object, [key: string]: any} => {
     return {
         server: 'https://cloud-gateway-test.53zaixian.com',
         onRequest: ({ request, options }: any) => {
-            const token = useMyCookie('token')
-            const agencyId = useMyCookie('agencyId')
+            const token = useCookieStorage('token')
+            const agencyId = useCookieStorage('agencyId')
             options.headers = options.headers || {}
             options.headers.platform = 'school'
             options.headers['auth-token'] = `Bearer ${token.value}`
