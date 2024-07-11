5e0f85bc4ba7673f4bd5f14701226ce9dea6ccfa
MengXianghan <1056811341@qq.com>
2024-07-11 19:03:57 +0800

    feat: 删除未测试代码

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,10 @@
 <template>
     <div>
-        <h2 class="text-xl font-semibold">首页</h2>
-        <template v-if="isLogin">
-            <a-space :size="8">
-                <a-avatar :src="userInfo.completeAvatar"></a-avatar>
-                <span>{{ userInfo.loginName }}</span>
-
-                <nuxt-link to="/login">切换账号</nuxt-link>
-            </a-space>
-        </template>
-
-        <template v-else>
-            <div>
-                <nuxt-link to="/login">登录</nuxt-link>
-            </div>
-        </template>
+        首页
     </div>
 </template>
 
 <script setup lang="ts">
-const { isLogin, userInfo } = await useUserInfo()
 </script>
 
 <style scoped></style>
