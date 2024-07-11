d3f151deaf9c2bafdbce0f0096654957a6aee64d
MengXianghan <1056811341@qq.com>
2024-07-11 19:02:23 +0800

    feat: 增加默认值

--- composables/storage.ts
+++ composables/storage.ts
@@ -1,19 +1,19 @@
 import storageConfig from '~/configs/storage'
 import { useStorage } from '@vueuse/core'
 
-export const useLocalStorage = (name: string, value: any) => {
+export const useLocalStorage = (name: string, value: any = null) => {
     const { namespace } = storageConfig()
     name = namespace ? `${namespace}${name}` : name
     return useStorage(name, value, localStorage)
 }
 
-export const useSessionStorage = (name: string, value: any) => {
+export const useSessionStorage = (name: string, value: any = null) => {
     const { namespace } = storageConfig()
     name = namespace ? `${namespace}${name}` : name
     return useStorage(name, value, sessionStorage)
 }
 
-export const useCookieStorage = (name: string, options: any) => {
+export const useCookieStorage = (name: string, options?: any) => {
     const { namespace } = storageConfig()
     name = namespace ? `${namespace}${name}` : name
     return useCookie(name, options)
