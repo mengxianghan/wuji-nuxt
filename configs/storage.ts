23f250553151f84e14d355cab038155b384d2f5c
MengXianghan <1056811341@qq.com>
2024-07-11 15:04:16 +0800

    feat: 删除废弃的文件和配置

--- configs/storage.ts
+++ configs/storage.ts
@@ -1,10 +1,8 @@
 export default () => {
     const app = useRuntimeConfig()
     const namespace = app.public.storageNamespace as string
-    const domain = app.public.storageDomain as string
 
     return {
         namespace,
-        domain,
     }
 }
