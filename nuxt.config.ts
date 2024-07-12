568f871119d53781db10c53f4f733f4e962e4c7e
MengXianghan <1056811341@qq.com>
2024-07-12 19:02:12 +0800

    style: 格式化代码

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-04-03',
-    devtools: { enabled: false },
-    modules: [
-        '@pinia/nuxt',
-        '@pinia-plugin-persistedstate/nuxt',
-        '@nuxtjs/tailwindcss',
-        '@ant-design-vue/nuxt',
-    ],
-    app: {
-        head: {
-            title: 'Wuji Nuxt!',
-        },
-    },
-    runtimeConfig: {
-        public: {
-            apiBase: '',
-            storageNamespace: 'wuji_nuxt_',
-        },
-    },
-    piniaPersistedstate: {
-        storage: 'cookies',
-    },
-    antd: {
-        extractStyle: true,
-    },
+  devtools: { enabled: false },
+
+  modules: [
+      '@pinia/nuxt',
+      '@pinia-plugin-persistedstate/nuxt',
+      '@nuxtjs/tailwindcss',
+      '@ant-design-vue/nuxt',
+  ],
+
+  app: {
+      head: {
+          title: 'Wuji Nuxt!',
+      },
+  },
+
+  runtimeConfig: {
+      public: {
+          apiBase: '',
+          storageNamespace: 'wuji_nuxt_',
+      },
+  },
+
+  piniaPersistedstate: {
+      storage: 'cookies',
+  },
+
+  antd: {
+      extractStyle: true,
+  },
+
+  compatibilityDate: '2024-07-12',
 })
