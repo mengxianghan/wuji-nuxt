ba84cfdaaa9ad9406c99c1b52e49b058b90d6645
mengxianghan <1056811341@qq.com>
2024-07-14 20:44:30 +0800

    feat: 解决冲突

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-04-03',
-    devtools: { enabled: false },
-    modules: [
-        '@pinia/nuxt',
-        '@pinia-plugin-persistedstate/nuxt',
-        '@nuxtjs/tailwindcss',
-        '@ant-design-vue/nuxt',
+  devtools: { enabled: false },
+
+  modules: [
+      '@pinia/nuxt',
+      '@pinia-plugin-persistedstate/nuxt',
+      '@nuxtjs/tailwindcss',
+      '@ant-design-vue/nuxt',
         '@nuxt/eslint',
-    ],
-    app: {
-        head: {
-            title: 'Wuji Nuxt!',
-        },
-    },
-    runtimeConfig: {
-        public: {
-            apiBase: '',
-            storageNamespace: 'wuji_nuxt_',
-        },
-    },
-    piniaPersistedstate: {
-        storage: 'cookies',
-    },
-    antd: {
-        extractStyle: true,
-    },
+  ],
+
+  app: {
+      head: {
+          title: 'Wuji Nuxt!',
+      },
+  },
+
+  runtimeConfig: {
+      public: {
+          apiBase: '',
+          storageNamespace: 'wuji_nuxt_',
+      },
+  },
+
+  antd: {
+      extractStyle: true,
+  },
+
+  compatibilityDate: '2024-07-12',
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
+    <div>首页</div>
     <div>
-        首页
+        <a-button type="primary" @click="refresh()">刷新</a-button>
+    </div>
+    <div>
+        <pre><code>{{ dynamic?.data }}</code></pre>
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+const { data: dynamic, refresh } = useAsyncData('list', async () =>
+    useRequest('school').post(
+        '/data',
+        { id: 1 },
+        { headers: { Test: 'test custom option' } }
+    )
+)
 </script>
 
 <style scoped></style>
