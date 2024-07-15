f26bcfcd4198c7c2e73e1439e99793e8b0586a43
MengXianghan <1056811341@qq.com>
2024-07-15 09:03:49 +0800

    chore: prettier 校验

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default withNuxt({
+    ...eslintConfigPrettier,
+    ...eslintPluginPrettierRecommended,
     files: ['**/*.ts', '**/*.vue', '**/*.js'],
     rules: {
         'vue/no-multiple-template-root': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        'prettier/prettier': 'error',
     },
 })
